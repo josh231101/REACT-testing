5fa1573a2d5b0be2ca6f68b901adc9e0658f65d8
Josue Arreola <47585614+josh231101@users.noreply.github.com>
2021-01-06 19:01:51 -0600

    Use DOM testing library for querying the dom Instead of writing root.querySelector and so on we can import our getQueriesForElement from testing library DOM to make the code less repetitive.

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { getQueriesForElement } from "@testing-library/dom";
 
 import App from "./App";
 
@@ -7,10 +8,16 @@ test("renders the correct content", () => {
   const root = document.createElement("div");
   ReactDOM.render(<App />, root);
 
+  const { getByText, getByLabelText } = getQueriesForElement(root);
   // Use DOM APIs querySelector to make assertions
-  expect(root.querySelector("h1").textContent).toBe("TODOs");
-  expect(root.querySelector("label").textContent).toBe(
-    "What needs to be done?"
-  );
-  expect(root.querySelector("button").textContent).toBe("Add #1");
+  //  expect(root.querySelector("h1").textContent).toBe("TODOs");
+  //  expect(root.querySelector("label").textContent).toBe(
+  //    "What needs to be done?"
+  //  );
+  //  expect(root.querySelector("button").textContent).toBe("Add #1");
+
+  // Using react testing library to avoid the repetition seen before
+  expect(getByText("TODOs")).not.toBeNull();
+  expect(getByLabelText("What needs to be done?")).not.toBeNull();
+  expect(getByText("Add #1")).not.toBeNull();
 });
