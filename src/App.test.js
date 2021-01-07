0db3433f6f2212694fe3676408f00f4c24ffd401
Josue Arreola <47585614+josh231101@users.noreply.github.com>
2021-01-06 20:56:34 -0600

    Testing Async Code

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,28 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 
 import App from "./App";
+import { api } from "./api";
 
-test("renders the correct content", () => {
-  const { getByText, getByLabelText } = render(<App />);
+// Normally you can mock entire module using jest.mock('./api);
+const mockCreateItem = (api.createItem = jest.fn());
 
-  expect(getByText("TODOs"));
-  expect(getByLabelText("What needs to be done?"));
-  expect(getByText("Add #1"));
-});
+test("allows users to add items to their list", async () => {
+  const todoText = "Todo new item";
+  mockCreateItem.mockResolvedValueOnce({ id: 123, text: todoText });
 
-test("allows users to add items to their list", () => {
   const { getByText, getByLabelText } = render(<App />);
 
   const input = getByLabelText("What needs to be done?");
-  fireEvent.change(input, { target: { value: "New item to TODO list" } });
-  fireEvent.click(getByText("Add #1"));
+  const button = getByText("Add #1");
+  fireEvent.change(input, { target: { value: todoText } });
+  fireEvent.click(button);
 
-  getByText("New item to TODO list");
-  getByText("Add #2");
+  await wait(() => getByText(todoText));
+
+  expect(mockCreateItem).toBeCalledTimes(1);
+  expect(mockCreateItem).toBeCalledWith(
+    "/items",
+    expect.objectContaining({ text: todoText })
+  );
 });
